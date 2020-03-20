e477d9e0a8168ef674a5d31ba445da6e6cbbdd21
S1V1R0 <carlos.csivira@gmail.com>
2020-03-19 20:13:48 -0400

    Proyect Done

--- shell_remoto_cli.py
+++ shell_remoto_cli.py
@@ -2,8 +2,10 @@ import os
 import sys
 import socket
 
-HEADER_SIZE = 16
+# Message header size and message size
+HEADER_SIZE = 32
 
+# Get the user input from prompt
 def get_input(msg: str):
 	try:
 		return input(msg)
@@ -11,22 +13,27 @@ def get_input(msg: str):
 		print (os.linesep + "user quit.")
 		sys.exit(0)
 
+# Set the correct format of the message to be sended
 def message_format(message: str, h_size: int):
-	return (f'{len(message):<{HEADER_SIZE}}' + message)
+	return ('{msg:<{sz}}'.format(msg = len(message), sz = h_size) + message)
 
+# Make client requests and wait for server responses
 def cli(ip: str, port: int):
 	while True:
+		# Creating the socket TCP
 		client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		message = get_input(">")
+
+		# Making the conection with the server
 		client_socket.connect((ip, port))
 		client_socket.setblocking(False)
-
-		message = get_input(">")
 
 		if message:
 			message = message
 			message = message_format(message, HEADER_SIZE)
 			client_socket.send(message.encode('utf-8'))
 
+		# Waiting for the response from server
 		is_header = False
 		full_message = ''
 		message_size = 0
@@ -37,10 +44,10 @@ def cli(ip: str, port: int):
 				continue
 			
 			if is_header: 
-				full_message += response.decode('utf-8').strip()	
+				full_message += response.decode('utf-8')
 				message_size -= HEADER_SIZE
 			else:
-				message_size = int(response.decode('utf-8').strip(' '))
+				message_size = int(response.decode('utf-8').strip())
 				is_header = True
 				continue
 
@@ -48,12 +55,15 @@ def cli(ip: str, port: int):
 				print(full_message)
 				break
 
+# User error output
 def badUse(message: str):
 	print(message)
 	print("Expected use: shell_remoto_cli -i <dir-ip> -p <puerto_svr>")
 	return 0
 
+# User error output
 def main():
+	# Check correctness of the input
 	if len(sys.argv) < 5:
 		badUse("Not enough arguments")
 		return 1
@@ -63,6 +73,7 @@ def main():
 	secondFlag	= sys.argv[3]
 	secondParam	= sys.argv[4]
 
+	# Run client
 	if firstFlag == "-i" and secondFlag == "-p":
 		cli(str(firstParam),int(secondParam))
 	elif firstFlag == "-p" and secondFlag == "-i":
--- shell_remoto_svr.py
+++ shell_remoto_svr.py
@@ -3,10 +3,14 @@ import sys
 import socket
 from signal import signal, SIGINT
 
+# Server locar IP
 IP = "127.0.0.1"
-HEADER_SIZE = 16
-MAX_NUMBER_SOLICITUDES = 8
+# Message header size and message size
+HEADER_SIZE = 32
+# Maximun number of supported requests
+MAX_NUMBER_REQUESTS = 8
 
+# Exit secuence
 def handler(signal_received, frame):
     # Handle any cleanup here
     print('\nSIGINT or CTRL-C detected. Shutting down the server')
@@ -18,22 +22,24 @@ def get_output(file_name: str):
 		file = open(file_name, 'r')
 		content = str(file.read())
 		file.close()
-		os.system("rm .output*")
+		os.system("rm " + file_name)
 		return content.encode('utf-8')
 	except:
 		response = "The Shell couldn't execute the command properly"
 		return response.encode('utf-8')
 
+# Set the correct format of the message to be sended
 def message_format(message: str, h_size: int):
-	return (f'{len(message):<{HEADER_SIZE}}' + message)
+	return ('{msg:<{sz}}'.format(msg = len(message), sz = h_size) + message)
 
+# Listen messages from clients and give responses
 def svr(port: int):
 	# Creating the server socket
 	server_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_socket.bind((IP, port))
 
 	# Listen for clients requests
-	server_socket.listen(MAX_NUMBER_SOLICITUDES)
+	server_socket.listen(MAX_NUMBER_REQUESTS)
 
 	print('Running. Press CTRL-C to exit.')
 	print('Waiting for clients...')
@@ -42,6 +48,7 @@ def svr(port: int):
 		client_socket, address = server_socket.accept()
 		print(f"Connection from {address} has been established.")
 
+		# Waiting for the response from server
 		is_header = False
 		command = ''
 		message_size = 0
@@ -52,11 +59,10 @@ def svr(port: int):
 				continue
          
 			if is_header:
-				command += response.decode('utf-8').strip()
+				command += response.decode('utf-8')
 				message_size -= HEADER_SIZE
 			else:
-				print(int(response.decode('utf-8').strip(' ')))
-				message_size = int(response)
+				message_size = int(response.decode('utf-8').strip())
 				is_header = True
 				continue
         
@@ -72,12 +78,15 @@ def svr(port: int):
 		client_socket.send(response.encode('utf-8'))
 		client_socket.close()
 
+# User error output
 def badUse(message: str):
 	print(message)
 	print("Expected use: shell_remoto_svr -p <puerto_svr>")
 	return 0
 
+# Main function of the program
 def main():
+	# Check correctness of the input
 	if len(sys.argv) < 3:
 		badUse("Not enough arguments")
 		return 1
@@ -85,6 +94,7 @@ def main():
 	firstFlag	= sys.argv[1]
 	firstParam	= sys.argv[2]
 
+	# Run the server
 	if firstFlag == "-p":
 		svr(int(firstParam))
 	else:
